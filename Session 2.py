68eb8bf5214d6966d00446811e1a976c6b4c49f0
Juveh <brianotieno292470@gmail.com>
2022-05-11 08:15:12 +0300

    first commit

---
+++ Session 2.py
@@ -0,0 +1,16 @@
+print ("This is git information")
+# fruits = ["Banana", "oranges", "apples"]
+# integers = 23
+# decimals = 23.90
+# full_name = "brian omondi"
+#
+# dcm = 65.90
+# print(dcm)
+#
+# print(fruits)
+# print(type(fruits))
+# print(type(integers))
+# print(type(decimals))
+# print(type(full_name))
+
+
